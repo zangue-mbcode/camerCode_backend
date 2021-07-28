fea3dbe7263fa67f6f1eca2e82a54df2b2572a9a
zangue-mbcode <zanulrich@gmail.com>
2021-07-28 15:57:13 +0100

    test backend

---
+++ controllers/post.js
@@ -0,0 +1,93 @@
+const Post = require('../models/post');
+
+exports.createPost = (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    htmlContent: req.body.htmlContent,
+    userId: req.body.userId
+  });
+  console.log('post : ', post)
+  post.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.getOnePost = (req, res, next) => {
+  Post.findOne({
+    _id: req.params.id
+  }).then(
+    (post) => {
+      res.status(200).json(post);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.modifyPost = (req, res, next) => {
+  const post = new Post({
+    _id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    htmlContent: req.body.htmlContent,
+    userId: req.body.userId
+  });
+  Post.updateOne({_id: req.params.id}, post).then(
+    () => {
+      res.status(201).json({
+        message: 'Post updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.deletePost = (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.getAllPost = (req, res, next) => {
+  Post.find().then(
+    (things) => {
+      res.status(200).json(things);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
---
+++ routes/post.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+const postCtrl = require('../controllers/post');
+
+router.get('/', auth, postCtrl.getAllPost);
+router.post('/', auth, postCtrl.createPost);
+router.get('/:id', auth, postCtrl.getOnePost);
+router.put('/:id', auth, postCtrl.modifyPost);
+router.delete('/:id', auth, postCtrl.deletePost);
+
+module.exports = router;
